c3008f6305dcea0370d09eefe4e63a067e685cd4
Cherry on a cake <ninjaxxxflame@gmail.com>
2024-07-01 13:04:13 +0700

    MainModel.cs added, window gridded.

---
+++ Ship/MainModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ship
+{
+    public class MainModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) =>
+        {
+
+        };
+
+
+    }
+
+}
